8c7d8b7e1a4f7e867fea71a7990ca710fb1ad3e0
League Student <league@iMac.attlocal.net>
2019-06-09 14:35:43 -0700

    working

--- src/_01_Bit_Shifting/BitShifter.java
+++ src/_01_Bit_Shifting/BitShifter.java
@@ -3,11 +3,13 @@ package _01_Bit_Shifting;
 public class BitShifter {
 	public static void main(String[] args) {
 		// 1. Jot down the value of num in binary.
-		int num = 3;
-		int numShifted = num >> 1;
+		int num = 13;
+		//1101
+		int numShifted = num << 3;
 		
 		// 2. Print the value of numShifted, and convert that value to binary.
 		System.out.println(numShifted);
+		
 		// 3. Compare the two binary values. Can you figure out what the << operator is for?
 		
 		// 4. Try shifting num 3 places.
--- src/_03_Printing_Binary/BinaryPrinter.java
+++ src/_03_Printing_Binary/BinaryPrinter.java
@@ -5,19 +5,37 @@ public class BinaryPrinter {
 	//Don't be afraid to use the methods that are already complete to finish the others.
 	//Create a main method to test your methods.
 	
-	public void printByteBinary(byte b) {
-		
+	public static void printByteBinary(byte b) {
+		/*int num = b;
+		String sum = "";
+		for (int i = 8; i > 0; i--) {
+		if(num>>i == 1) {
+			sum = sum + "1"; 
+			if(!(i==1)) {
+			num = num - 2^8;
+			}
+			}
+		else {
+			sum = sum + "0"; 
+		}
+		}
+		System.out.println(sum);*/
+		System.out.println(Integer.toBinaryString(b));
 	}
 	
 	public void printShortBinary(short s) {
-		
+		System.out.println(Integer.toBinaryString(s));
 	}
 	
 	public void printIntBinary(int i) {
-		
+		System.out.println(Integer.toBinaryString(i));
 	}
 	
 	public void printLongBinary(long l) {
-		
+		System.out.println(Integer.toBinaryString((int) l));
+		//:-) also mr Mike taught me how to cheat the system
+	}
+	public static void main(String[] args) {
+		printByteBinary((byte) 100);
 	}
 }

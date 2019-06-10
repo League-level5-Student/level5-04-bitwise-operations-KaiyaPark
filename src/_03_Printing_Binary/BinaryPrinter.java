d5d9acb1d03cfe9bf1f1c187622f2d9ed3b101a8
League Student <league@WTS14-3.attlocal.net>
2019-06-09 17:27:52 -0700

    Base64Decoder(IP)

--- src/_03_Printing_Binary/BinaryPrinter.java
+++ src/_03_Printing_Binary/BinaryPrinter.java
@@ -6,36 +6,69 @@ public class BinaryPrinter {
 	//Create a main method to test your methods.
 	
 	public static void printByteBinary(byte b) {
-		/*int num = b;
-		String sum = "";
-		for (int i = 8; i > 0; i--) {
-		if(num>>i == 1) {
-			sum = sum + "1"; 
-			if(!(i==1)) {
-			num = num - 2^8;
-			}
-			}
-		else {
-			sum = sum + "0"; 
+		
+		String s = "";
+		String str = "";
+		byte a = 0b1;
+		for (int i = 0; i < 8; i++) {
+			s += (b&a) >> i;
+			a = (byte) (a << 1);
 		}
+		for (int i = 7; i > -1; i--) {
+			str += s.charAt(i);
 		}
-		System.out.println(sum);*/
+		System.out.println(str);
 		System.out.println(Integer.toBinaryString(b));
 	}
 	
-	public void printShortBinary(short s) {
+	public static void printShortBinary(short s) {
+		String st = "";
+		String str = "";
+		short a = 0b1;
+		for (int i = 0; i < 16; i++) {
+			st += (s&a) >> i;
+			a = (short) (a << 1);
+		}
+		for (int i = 15; i > -1; i--) {
+			str += st.charAt(i);
+		}
+		System.out.println(str);
 		System.out.println(Integer.toBinaryString(s));
 	}
 	
-	public void printIntBinary(int i) {
+	public static void printIntBinary(int i) {
+		String st = "";
+		String str = "";
+		int a = 0b1;
+		for (int is = 0; is < 32; is++) {
+			st += (i&a) >> is;
+			a = (int) (a << 1);
+		}
+		for (int is = 31; is > -1; is--) {
+			str += st.charAt(is);
+		}
+		System.out.println(str);
 		System.out.println(Integer.toBinaryString(i));
 	}
 	
-	public void printLongBinary(long l) {
+	public static void printLongBinary(long l) {
+		String st = "";
+		String str = "";
+		long a = 0b1;
+		for (int is = 0; is < 64; is++) {
+			st += (l&a) >> is;
+			a = (long) (a << 1);
+		}
+		for (int is = 63; is > -1; is--) {
+			str += st.charAt(is);
+		}
+		System.out.println(str);
 		System.out.println(Integer.toBinaryString((int) l));
-		//:-) also mr Mike taught me how to cheat the system
 	}
 	public static void main(String[] args) {
 		printByteBinary((byte) 100);
+		printShortBinary((short) 11111);
+		printIntBinary((int) 1234567890);
+		printLongBinary((long) 999999999);
 	}
 }
